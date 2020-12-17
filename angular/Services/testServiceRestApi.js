14b767900d14e120108a7749505447b8e2795702
Ladavi <dawid.poz@wp.pl>
2020-12-17 10:41:28 +0100

    Zamiana factory na service

--- angular/Services/testServiceRestApi.js
+++ angular/Services/testServiceRestApi.js
@@ -1,14 +1,16 @@
-app.factory('serviceRestApi', ['$http', function ($http) {
-    return {
-        getData: function() {
-          $http.get("https://randomuser.me/api/").then(
-            function successCallback(response) {
-              console.log(response.data);
-            },
-            function errorCallback(response) {
-              console.log("Unable to perform get request");
-            }
-          );
-        },
-    };
+app.service('serviceRestApi', ['$http', function ($http) {
+
+  this.getData = function() {
+    $http.get("https://randomuser.me/api/").
+    then(
+      function successCallback(response) {
+        console.log(response.data);
+        return response.data;
+      },
+      function errorCallback(response) {
+        console.log("Unable to perform get request");
+      }
+    );
+  }
+
 }])
