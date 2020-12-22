e63335e2aefe2dcf1b7db7d8374d051f9a1aeda6
Ladavi <dawid.poz@wp.pl>
2020-12-22 11:43:40 +0100

    request do tabelki, przerobiony service

--- angular/Controllers/adminTableController.js
+++ angular/Controllers/adminTableController.js
@@ -1,8 +1,14 @@
 app.controller("adminTableController", ['$scope', 'adminGetTableRestApi', function($scope, adminGetTableRestApi) {
     $scope.home = "This is the homepage";
-  
+    $scope.requestData = "";
+    
     $scope.getRequest = function() {
-      console.log("I've been pressed!");
-      adminGetTableRestApi.getData();
+      adminGetTableRestApi.getData().then(
+        function(response) {
+          $scope.requestData = response.data;
+      }
+      );
+      //console.log(adminGetTableRestApi.getData());
+      //$scope.requestData = adminGetTableRestApi.getData();
     };
   }]);
