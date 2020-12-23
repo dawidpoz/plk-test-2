76e4b5ebece8177a4b84f863037c43952a9ad5ca
Ladavi <dawid.poz@wp.pl>
2020-12-23 15:58:41 +0100

    get panel admina stations

--- angular/Controllers/adminTableController.js
+++ angular/Controllers/adminTableController.js
@@ -1,6 +1,11 @@
-app.controller("adminTableController", ['$scope', 'adminGetTableRestApi', function($scope, adminGetTableRestApi) {
+app.controller("adminTableController", ['$scope', 'adminGetTableRestApi', 'serviceGetListOfStations', function($scope, adminGetTableRestApi, serviceGetListOfStations) {
     $scope.home = "This is the homepage";
     $scope.requestData = "";
+    $scope.stations = "";
+
+    $scope.init = function(){
+      this.getStations();
+  } 
     
     $scope.getRequest = function() {
       adminGetTableRestApi.getData().then(
@@ -10,5 +15,13 @@ app.controller("adminTableController", ['$scope', 'adminGetTableRestApi', functi
       );
       console.log("click");
       //$scope.requestData = adminGetTableRestApi.getData();
+    };
+
+    $scope.getStations = function() {
+      serviceGetListOfStations.getData().then(
+        function(response) {
+          $scope.stations = response.data;
+      }
+      );
     };
   }]);
